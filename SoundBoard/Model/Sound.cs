2d279ad606a68c58891aa55b4995e176aa2f704b
Kal <info@kalacademy.org>
2022-06-05 12:10:02 -0700

    Initial commit

---
+++ SoundBoard/Model/Sound.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.Model
+{
+    public enum SoundCategory
+    {
+        Animals,
+        Cartoons,
+        Taunts,
+        Warnings
+    }
+    internal class Sound
+    {
+        public string Name { get; set; }
+        public SoundCategory Category { get; set; }
+        public string AudioFile { get; set; }
+        public string ImageFile { get; set; }
+    }
+}
